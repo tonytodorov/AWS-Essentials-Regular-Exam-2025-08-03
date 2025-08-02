c2064e318037059c2c67df8fce31fa315d793715
Tony Todorov <tonytodorov@protonmail.com>
2025-08-02 12:42:35 +0300

    initial commit

---
+++ test/aws-essentials-regular-exam-2025-08-03.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsEssentialsRegularExam20250803 from '../lib/aws-essentials-regular-exam-2025-08-03-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-essentials-regular-exam-2025-08-03-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsEssentialsRegularExam20250803.AwsEssentialsRegularExam20250803Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
